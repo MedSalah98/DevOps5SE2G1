ab51a468e03ed9b13a2844aa15d766f98ec5f537
oussama.issaoui1@esprit.tn <57869879+oussamaissaoui@users.noreply.github.com>
2021-11-15 23:39:36 +0000

    pom.xml - test

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -2,7 +2,7 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class TimesheetApplicationTests {
 
@@ -11,3 +11,4 @@ class TimesheetApplicationTests {
 	}
 
 }
+*/
